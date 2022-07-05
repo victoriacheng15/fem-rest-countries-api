fa239ecc08e48149916d693172dbcd97ed8223a9
Victoria Cheng <victoria.cheng15@gmail.com>
2022-07-05 16:05:20 -0400

    add header compoment and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import HeaderBar from './componments/header/Header';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const setMode = () => setDarkMode(!darkMode);
+
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Rest Countries API app</h1>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum itaque sunt fuga delectus hic, alias corrupti minus suscipit quo deserunt.</p>
-    </>
+    <div className={`min-h-screen font-nuito ${darkMode && 'dark'}`}>
+      <HeaderBar onClick={setMode} isDark={darkMode} />
+      <h1 className="text-3xl font-bold text-blue-900">
+        Rest Countries API app {darkMode ? 'dark' : 'light'}
+      </h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum itaque
+        sunt fuga delectus hic, alias corrupti minus suscipit quo deserunt.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum itaque
+        sunt fuga delectus hic, alias corrupti minus suscipit quo deserunt.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum itaque
+        sunt fuga delectus hic, alias corrupti minus suscipit quo deserunt.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum itaque
+        sunt fuga delectus hic, alias corrupti minus suscipit quo deserunt.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum itaque
+        sunt fuga delectus hic, alias corrupti minus suscipit quo deserunt.
+      </p>
+    </div>
   );
 }
 
