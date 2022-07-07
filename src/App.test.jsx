d3d540d35f4a15190bf04858e165109c341762ed
Victoria Cheng <victoria.cheng15@gmail.com>
2022-07-07 16:23:09 -0400

    renmae test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,7 +4,7 @@ import App from './App';
 import { render, screen, userEvent } from '../test-utils';
 
 describe('Simple working test', () => {
-  it('have dark mode text after button is fired', () => {
+  it('have light mode text by default', () => {
     render(<App />);
     const button = screen.getByRole('button');
     expect(button).toHaveTextContent('light mode');
@@ -14,6 +14,7 @@ describe('Simple working test', () => {
     render(<App />);
     const button = screen.getByRole('button');
     userEvent.click(button);
-    expect(await screen.findByText(/dark mode/i)).toBeInTheDocument();
+    const buttonTextContent = screen.findByText(/dark mode/i)
+    expect(await buttonTextContent).toHaveTextContent('dark mode');
   });
 });
