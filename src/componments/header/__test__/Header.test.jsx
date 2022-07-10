27c0d9e2bc1bb1daf690f0386e92d05803371bd3
Victoria Cheng <victoria.cheng15@gmail.com>
2022-07-10 17:22:29 -0400

    change test name for header

--- src/componments/header/__test__/Header.test.jsx
+++ src/componments/header/__test__/Header.test.jsx
@@ -4,9 +4,15 @@ import HeaderBar from '../Header';
 import { render, screen } from '../../../../test-utils';
 
 describe('header', () => {
-  it('should have the heading', () => {
+  it('should have the h1 in the page', () => {
     render(<HeaderBar />);
     const heading = screen.getByText(/where in the world?/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('should have the textContent "Where in the world?"', () => {
+    render(<HeaderBar />);
+    const heading = screen.getByTestId('heading');
     expect(heading).toHaveTextContent('Where in the world?');
   });
 });
