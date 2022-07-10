0a4e5278139e06f80a20cef14c3ee5c65f112ce8
Victoria Cheng <victoria.cheng15@gmail.com>
2022-07-10 17:21:47 -0400

    add testid for RTL

--- src/componments/header/Header.jsx
+++ src/componments/header/Header.jsx
@@ -5,7 +5,10 @@ function HeaderBar({ onClick, isDark }) {
   return (
     <header className="py-16 bg-lightGray-700 dark:bg-darkBlue-700 text-darkBlue-900 dark:text-lightGray-700">
       <div className="flex items-center justify-between w-11/12 mx-auto">
-        <h1 className="text-[20px] font-semibold md:text-[30px]">
+        <h1
+          data-testid="heading"
+          className="text-[20px] font-semibold md:text-[30px]"
+        >
           Where in the world?
         </h1>
         <button
