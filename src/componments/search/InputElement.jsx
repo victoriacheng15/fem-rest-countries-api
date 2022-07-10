6b40cafe3fc159513a48b1f338935c2d9a95bcca
Victoria Cheng <victoria.cheng15@gmail.com>
2022-07-10 17:26:24 -0400

    add handleInput for input element

--- src/componments/search/InputElement.jsx
+++ src/componments/search/InputElement.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { IoSearchSharp } from 'react-icons/io5';
 
-function InputElement() {
+function InputElement({ input, handleInput }) {
   return (
     <div className="relative flex items-center rounded-lg bg-lightGray-700 dark:bg-darkBlue-700">
       <IoSearchSharp className="absolute ml-1 text-2xl text-lightGray-900 dark:text-lightGray-700" />
       <input
-        className="w-full h-full p-4 pl-8 rounded-lg text-lightGray-900 dark:bg-darkBlue-700 dark:placeholder-lightGray-700 outline-style"
+        value={input}
+        onChange={handleInput}
+        className="w-full h-full p-4 pl-8 rounded-lg text-lightGray-900 dark:bg-darkBlue-700 dark:placeholder-lightGray-700 outline-style md:w-[480px]"
         type="text"
         placeholder="Search for a country..."
       />
