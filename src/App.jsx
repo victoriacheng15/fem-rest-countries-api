006f848807ba4896b8ef7eadcb36c5e80482b86e
Victoria Cheng <victoria.cheng15@gmail.com>
2022-07-14 13:41:14 -0400

    add classes for the div element

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
 import HeaderBar from './componments/header/Header';
 import Main from './componments/main/Main';
+import Footer from './componments/footer/Footer';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
-
   const setMode = () => setDarkMode(!darkMode);
 
   return (
-    <div className={`min-h-screen font-nuito ${darkMode && 'dark'}`}>
+    <div
+      className={`min-h-screen font-nuito flex flex-col bg-darkBlue-800 justify-center ${
+        darkMode && 'dark'
+      }`}
+    >
       <HeaderBar onClick={setMode} isDark={darkMode} />
       <Main />
+      <Footer />
     </div>
   );
 }
