75bb6eccce3c9a6fab1837c5dd7907b91ed30791
Victoria Cheng <victoria.cheng15@gmail.com>
2022-07-18 15:36:51 -0400

    add CountryCard test cases

--- src/componments/countries/__test__/Countries.test.jsx
+++ src/componments/countries/__test__/Countries.test.jsx
@@ -1,17 +1,43 @@
 import React from 'react';
-import { beforeEach, describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import Countries from '../Countries';
-import useFetchCountries from '../../../hook/useFetchApi';
+import CountryCard from '../CountryCard';
 
-// describe('counrties', () => {
-//   beforeEach(() => {
-//     const { countries } = useFetchCountries();
-//     render(<Countries countries={countries} />);
-//   });
-//   it('should have country cards', () => {
-//     // render(<Countries />);
-//     const cards = screen.queryAllByTestId('card');
-//     expect(cards).toBeInTheDocument();
-//   });
-// });
+const country = {
+  url: 'canada',
+  name: 'Canada',
+  captial: 'Ottawa',
+  flag: 'https://flagcdn.com/w320/ca.png',
+  population: 38005238,
+  region: 'Americas',
+};
+
+describe('country card', () => {
+  beforeEach(() => {
+    render(
+      <CountryCard
+        name={country.name}
+        countryCaptial={country.capital}
+        region={country.region}
+        flags={country.flag}
+        alt={country.name}
+        population={country.population}
+      />
+    );
+  });
+
+  it('should return Canada as the country name', async () => {
+    const countryName = screen.getByText(/canada/i);
+    expect(countryName).toHaveTextContent('Canada');
+  });
+
+  it('should return 38005238 as the country population', () => {
+    const countryPopulation = screen.getByText(/population/i);
+    expect(countryPopulation).toHaveTextContent(38005238);
+  });
+
+  it('should return Ottawa as the country captial', async () => {
+    const countryCaptial = screen.getByText(/captial/i);
+    expect(countryCaptial).toBeInTheDocument();
+  });
+});
