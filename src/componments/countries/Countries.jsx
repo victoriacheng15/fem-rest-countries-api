fd5e7755536496d81540c220a527d4b73283d901
Victoria Cheng <victoria.cheng15@gmail.com>
2022-07-18 15:36:33 -0400

    creat countrycard componment

--- src/componments/countries/Countries.jsx
+++ src/componments/countries/Countries.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CountryCard from './CountryCard';
 
 function Countries({ countries, loading }) {
   if (loading) {
@@ -8,25 +9,14 @@ function Countries({ countries, loading }) {
   return (
     <section className="grid items-center justify-center py-12 justify-items-center gap-14 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {countries.map(({ name, capital, region, flags, population }) => (
-        <article
-          data-testid="card"
-          key={name.common}
-          className="flex flex-col w-[300px] h-[340px] gap-8 bg-lightGray-700 rounded-lg dark:bg-darkBlue-700 overflow-hidden shadow-lg dark:shadow-gray-700"
-        >
-          <img
-            src={flags.png}
-            alt={name.official}
-            className="h-[160px] w-full object-fit"
-          />
-          <div className="p-2">
-            <h2 className="pb-2 font-extrabold text-darkBlue-900 dark:text-lightGray-800">
-              {name.common}
-            </h2>
-            <p className="card-text">Population: {population}</p>
-            <p className="card-text">Region: {region}</p>
-            <p className="card-text">Captial: {capital}</p>
-          </div>
-        </article>
+        <CountryCard
+          name={name.official}
+          countryCaptial={capital}
+          region={region}
+          flags={flags.png}
+          alt={name.official}
+          population={population}
+        />
       ))}
     </section>
   );
---
+++ src/componments/countries/CountryCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function CountryCard({ name, alt, countryCaptial, region, flags, population }) {
+  return (
+    <article
+      data-testid="card"
+      key={name}
+      className="flex flex-col w-[300px] h-[340px] gap-8 bg-lightGray-800 rounded-lg dark:bg-darkBlue-700 overflow-hidden shadow-lg shadow-gray-600 dark:shadow-gray-600"
+    >
+      <img src={flags} alt={alt} className="h-[160px] w-full object-fit" />
+      <div className="p-2">
+        <h2 className="pb-2 font-extrabold text-darkBlue-900 dark:text-lightGray-800">
+          {name}
+        </h2>
+        <p className="card-text">Population: {population}</p>
+        <p className="card-text">Region: {region}</p>
+        <p className="card-text">Captial: {countryCaptial}</p>
+      </div>
+    </article>
+  );
+}
+
+export default CountryCard;
