1ddf22daa713b48eae75652ca8aeefa85ccf62d8
Victoria Cheng <victoria.cheng15@gmail.com>
2022-09-12 13:38:10 -0600

    add context provider for countries

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,24 @@ import React, { useState } from 'react';
 import HeaderBar from './componments/header/Header';
 import Main from './componments/main/Main';
 import Footer from './componments/footer/Footer';
+import { CountriesProvider } from './context/CountriesContext';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
   const setMode = () => setDarkMode(!darkMode);
 
   return (
-    <div
-      className={`min-h-screen font-nuito flex flex-col bg-darkBlue-800 justify-center ${
-        darkMode && 'dark'
-      }`}
-    >
-      <HeaderBar onClick={setMode} isDark={darkMode} />
-      <Main />
-      <Footer />
-    </div>
+    <CountriesProvider>
+      <div
+        className={`min-h-screen font-nuito flex flex-col bg-darkBlue-800 justify-center ${
+          darkMode && 'dark'
+        }`}
+      >
+        <HeaderBar onClick={setMode} isDark={darkMode} />
+        <Main />
+        <Footer />
+      </div>
+    </CountriesProvider>
   );
 }
 
