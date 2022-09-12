6d1814e2b78ee912a525b7e9f37b136b4e31bac8
Victoria Cheng <victoria.cheng15@gmail.com>
2022-09-12 13:33:03 -0600

    add context to handle data

---
+++ src/context/CountriesContext.jsx
@@ -0,0 +1,55 @@
+import React, { createContext, useState, useEffect } from 'react';
+import useFetchCountries from '../hook/useFetchApi';
+
+const CountriesContext = createContext();
+
+export function CountriesProvider({ children }) {
+  const { countries, loading, list, setList } = useFetchCountries();
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectRegion, setSelectRegion] = useState('');
+  const [countriesPerPage] = useState(12);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handleSearch = (searchValue) => {
+    const value = searchValue.toLowerCase();
+    setSearchTerm(value);
+    const searchFiltered = countries.filter(({ name }) => {
+      const countryName = name.official.toLowerCase();
+      return countryName.includes(value);
+    });
+    setList(searchFiltered);
+  };
+
+  const handleSelect = (selectValue) => {
+    setSelectRegion(selectValue);
+    const selectFiltered = countries.filter(({ region }) =>
+      region.includes(selectValue)
+    );
+
+    setList(selectFiltered);
+  };
+
+  const idxOfLastCountries = currentPage * countriesPerPage;
+  const idxOfFirstCountries = idxOfLastCountries - countriesPerPage;
+  const currentCountries = list.slice(idxOfFirstCountries, idxOfLastCountries);
+
+  return (
+    <CountriesContext.Provider
+      value={{
+        countries,
+        loading,
+        list,
+        searchTerm,
+        selectRegion,
+        setCurrentPage,
+        handleSearch,
+        handleSelect,
+        currentCountries,
+      }}
+    >
+      {children}
+    </CountriesContext.Provider>
+  );
+}
+
+export default CountriesContext;
