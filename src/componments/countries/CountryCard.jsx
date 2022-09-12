39218ead9f6201c9547b02e632aab7d69c05315a
Victoria Cheng <victoria.cheng15@gmail.com>
2022-09-12 16:36:35 -0600

    fix display info place and add height for card

--- src/componments/countries/CountryCard.jsx
+++ src/componments/countries/CountryCard.jsx
@@ -4,9 +4,9 @@ function CountryCard({ name, alt, countryCaptial, region, flags, population }) {
   return (
     <article
       data-testid="card"
-      className="flex flex-col justify-between gap-8 overflow-hidden rounded-lg shadow-lg basis-72 grow-1 bg-lightGray-800 dark:bg-darkBlue-700 shadow-gray-600 dark:shadow-gray-600"
+      className="h-[375px] flex flex-col gap-4 overflow-hidden rounded-lg shadow-lg basis-72 grow-1 bg-lightGray-800 dark:bg-darkBlue-700 shadow-gray-600 dark:shadow-gray-600"
     >
-      <img src={flags} alt={alt} className="h-[160px] w-full object-fit" />
+      <img src={flags} alt={alt} className="h-[180px] w-full object-fit" />
       <div className="p-4">
         <h2 className="pb-2 font-extrabold text-darkBlue-900 dark:text-lightGray-800">
           {name}
