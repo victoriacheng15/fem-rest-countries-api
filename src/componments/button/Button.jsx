1baf225db27b01ec8a61feef744fd5db68d6e3e1
Victoria Cheng <victoria.cheng15@gmail.com>
2022-09-13 12:59:02 -0600

    refactor: remove unused code

--- src/componments/button/Button.jsx
+++ src/componments/button/Button.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 
-function Button({ dataType, onClick, children }) {
+function Button({ onClick, children }) {
   return (
     <button
       type="button"
-      data-type={dataType}
       className="text-5xl text-darkBlue-900 dark:text-lightGray-800"
       onClick={onClick}
     >
