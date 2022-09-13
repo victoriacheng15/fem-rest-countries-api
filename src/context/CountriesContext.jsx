83434a4ebf2e4c6f83929def950a712f1e4a2875
Victoria Cheng <victoria.cheng15@gmail.com>
2022-09-13 13:02:32 -0600

    fix: update useFetchCountries file path

--- src/context/CountriesContext.jsx
+++ src/context/CountriesContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState } from 'react';
-import useFetchCountries from '../hook/useFetchApi';
+import useFetchCountries from '../hook/useFetchCountries';
 
 const CountriesContext = createContext();
 
