7e35e56091b576e93ab0d018e8097c804cd3b5a6
Victoria Cheng <victoria.cheng15@gmail.com>
2022-09-13 14:37:49 -0600

    add country info into content

--- src/pages/CountryPage.jsx
+++ src/pages/CountryPage.jsx
@@ -10,7 +10,40 @@ function CountryPage() {
     <>
       <Link to="/">Back</Link>
       <section>
-        <h1> dummy page</h1>
+        {country.map(
+          ({
+            flags,
+            name,
+            population,
+            region,
+            subregion,
+            capital,
+            tld,
+            currencies,
+            languages,
+            borders,
+          }) => (
+            <article key={name.common}>
+              <img src={flags.png} alt={name.common} />
+              <h2>{name.common}</h2>
+              <div>
+                <p>Population: {population.toLocaleString()}</p>
+                <p>Region: {region}</p>
+                <p>Sub Region: {subregion}</p>
+                <p>Capital: {capital}</p>
+              </div>
+              <div>
+                <p>Top LeveL domain: {tld}</p>
+                <p>currencies: {Object.keys(currencies)}</p>
+                <p>languages: {languages.name}</p>
+              </div>
+              <div>
+                <h3>Border Countries:</h3>
+                <p>{borders.map((item) => item)}</p>
+              </div>
+            </article>
+          )
+        )}
       </section>
     </>
   );
