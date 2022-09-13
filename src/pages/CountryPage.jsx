9f10bccace26fa70e5f0b45f9798c69266f379ca
Victoria Cheng <victoria.cheng15@gmail.com>
2022-09-13 12:57:03 -0600

    refactor: move countries and countryPage to pages folder

---
+++ src/pages/CountryPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function CountryPage() {
+  return (
+    <>
+      <h1> dummy page</h1>
+      <h1> dummy page</h1>
+      <h1> dummy page</h1>
+      <h1> dummy page</h1>
+    </>
+  );
+}
+
+export default CountryPage;
