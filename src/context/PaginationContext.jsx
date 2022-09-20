3e6de71d8ced5c6104ff9ec5baddd288b1e6794c
Victoria Cheng <victoria.cheng15@gmail.com>
2022-09-20 11:48:17 -0600

    refactor: add handlePrev and handleNext functions and adjust props in value

--- src/context/PaginationContext.jsx
+++ src/context/PaginationContext.jsx
@@ -15,17 +15,46 @@ export function PaginationProvider({ children }) {
   const idxOfFirstCountries = idxOfLastCountries - countriesPerPage;
   const currentCountries = list.slice(idxOfFirstCountries, idxOfLastCountries);
 
+  const totalPages = Math.ceil(list.length / countriesPerPage); // 21
+  const pageNumbers = [...new Array(totalPages + 1).keys()].slice(1);
+  const displayPages = pageNumbers.slice(startPage, endPage);
+
+  const handleNext = (page) => {
+    const FIVE = displayPages.length;
+    const condition = page <= Math.floor(FIVE / 2);
+    if (condition) {
+      setCurrentPage(page + 1);
+      setStartPage(0);
+      setEndPage(5);
+    } else {
+      setCurrentPage(page >= totalPages ? totalPages : page + 1);
+      setStartPage(page >= totalPages - 2 ? totalPages - 5 : startPage + 1);
+      setEndPage(page >= totalPages ? totalPages : endPage + 1);
+    }
+  };
+
+  const handlePrev = (page) => {
+    const condition = page > totalPages - 2;
+    if (condition) {
+      setCurrentPage(page - 1);
+      setStartPage(condition ? totalPages - 5 : startPage - 1);
+      setEndPage(condition ? totalPages : endPage - 1);
+    } else {
+      setCurrentPage(page <= 1 ? 1 : page - 1);
+      setStartPage(startPage <= 0 ? 0 : startPage - 1);
+      setEndPage(endPage <= 5 ? 5 : endPage - 1);
+    }
+  };
+
   return (
     <PaginationContext.Provider
       value={{
         countriesPerPage,
         currentPage,
-        setCurrentPage,
-        startPage,
-        setStartPage,
-        endPage,
-        setEndPage,
+        displayPages,
         currentCountries,
+        handleNext,
+        handlePrev,
       }}
     >
       {children}
