51165b4893ff90e22b2f54d2925fc1daea53170c
Victoria Cheng <victoria.cheng15@gmail.com>
2022-09-20 11:25:21 -0600

    refactor: add paginationProvider

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,26 @@ import HeaderBar from './componments/header/Header';
 import Main from './componments/main/Main';
 import Footer from './componments/footer/Footer';
 import { CountriesProvider } from './context/CountriesContext';
+import { PaginationProvider } from './context/PaginationContext';
 
 function App() {
   const [darkMode, setDarkMode] = useReducer((prevMode) => !prevMode, false);
 
   return (
     <CountriesProvider>
-      <Router>
-        <div
-          className={`flex min-h-screen flex-col justify-center bg-darkBlue-800 font-nuito ${
-            darkMode && 'dark'
-          }`}
-        >
-          <HeaderBar onClick={setDarkMode} isDark={darkMode} />
-          <Main />
-          <Footer />
-        </div>
-      </Router>
+      <PaginationProvider>
+        <Router>
+          <div
+            className={`flex min-h-screen flex-col justify-center bg-darkBlue-800 font-nuito ${
+              darkMode && 'dark'
+            }`}
+          >
+            <HeaderBar onClick={setDarkMode} isDark={darkMode} />
+            <Main />
+            <Footer />
+          </div>
+        </Router>
+      </PaginationProvider>
     </CountriesProvider>
   );
 }
