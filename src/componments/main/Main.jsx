a14717d6ee6d5434f340f40568a63a2bf813d26b
Victoria Cheng <victoria.cheng15@gmail.com>
2022-09-20 11:30:35 -0600

    refactor: remove countriesList prop

--- src/componments/main/Main.jsx
+++ src/componments/main/Main.jsx
@@ -1,20 +1,17 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Countries from '../../pages/Countries';
-import CountriesContext from '../../context/CountriesContext';
+// import CountriesContext from '../../context/CountriesContext';
 import CountryPage from '../../pages/CountryPage';
 
 function Main() {
-  const { currentCountries } = useContext(CountriesContext);
+  // const { currentCountries } = useContext(CountriesContext);
 
   return (
     <main className="bg-lightGray-800 py-8 dark:bg-darkBlue-800">
       <div className="mx-auto w-11/12 max-w-7xl">
         <Routes>
-          <Route
-            path="/"
-            element={<Countries countriesList={currentCountries} />}
-          />
+          <Route path="/" element={<Countries />} />
           <Route path="/country/:code" element={<CountryPage />} />
         </Routes>
       </div>
