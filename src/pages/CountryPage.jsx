cca3ef6f6e7da1f20bcdfdd4d0df744eb5236533
Victoria Cheng <victoria.cheng15@gmail.com>
2022-09-21 13:22:37 -0600

    refactor: remove extra html elements and add Container componment

--- src/pages/CountryPage.jsx
+++ src/pages/CountryPage.jsx
@@ -4,6 +4,7 @@ import { IoArrowBackOutline } from 'react-icons/io5';
 import useFetchCountry from '../hook/useFetchCountry';
 import Spinner from '../componments/CountryCard/Spinner';
 import Paragraph from '../componments/Paragraph';
+import Container from '../componments/Container';
 
 function CountryPage() {
   const { code } = useParams();
@@ -19,79 +20,83 @@ function CountryPage() {
           <IoArrowBackOutline /> back
         </div>
       </Link>
-      <section className="mt-20 flex items-center justify-center">
-        {country.map(
-          ({
-            flags,
-            name,
-            population,
-            region,
-            subregion,
-            capital,
-            tld,
-            currencies,
-            borders,
-            languages,
-          }) => (
-            <article
-              key={name.common}
-              className="flex flex-col rounded-lg border-2 border-gray-500 p-2"
-            >
-              <div className="w-full pb-5">
-                <img className="w-full" src={flags.png} alt={name.common} />
-              </div>
-              <h2 className="my-5 text-lg font-bold  text-darkBlue-900 dark:text-lightGray-800">
+      {country.map(
+        ({
+          flags,
+          name,
+          population,
+          region,
+          subregion,
+          capital,
+          tld,
+          currencies,
+          borders,
+          languages,
+        }) => (
+          <section
+            key={name.common}
+            className="mt-20 flex w-full flex-col items-center justify-center gap-10 lg:flex-row"
+          >
+            <img
+              className="w-full max-w-[550px] flex-1 border-2 border-lightGray-900"
+              src={flags.png}
+              alt={name.common}
+            />
+            <div className="flex-1">
+              <h2 className="my-5 text-xl font-bold text-darkBlue-900 dark:text-lightGray-800 lg:text-2xl">
                 {name.common}
               </h2>
-              <div className="flex flex-col gap-3 py-5">
-                <Paragraph
-                  title="population"
-                  content={population.toLocaleString()}
-                />
-                <Paragraph title="region" content={region} />
-                <Paragraph title="sub region" content={subregion} />
-                <Paragraph title="capital" content={capital} />
-              </div>
-              <div className="flex flex-col gap-3 py-5">
-                <Paragraph
-                  title="top level domain"
-                  content={tld.map((elm) => elm).join(', ')}
-                />
-                <Paragraph
-                  title="currencies"
-                  content={getInput(currencies)
-                    .map((currency) => currency.name)
-                    .join(', ')}
-                />
-                <Paragraph
-                  title="languages"
-                  content={getInput(languages)
-                    .map((lang) => lang)
-                    .join(', ')}
-                />
+              <div className="lg:flex lg:justify-between lg:gap-4">
+                <Container>
+                  <Paragraph
+                    title="population"
+                    content={population.toLocaleString()}
+                  />
+                  <Paragraph title="region" content={region} />
+                  <Paragraph title="sub region" content={subregion} />
+                  <Paragraph title="capital" content={capital} />
+                </Container>
+                <Container>
+                  <Paragraph
+                    title="top level domain"
+                    content={tld.map((elm) => elm).join(', ')}
+                  />
+                  <Paragraph
+                    title="currencies"
+                    content={getInput(currencies)
+                      .map((currency) => currency.name)
+                      .join(', ')}
+                  />
+                  <Paragraph
+                    title="languages"
+                    content={getInput(languages)
+                      .map((lang) => lang)
+                      .join(', ')}
+                  />
+                </Container>
               </div>
               <div className="flex flex-col gap-3 py-5">
                 <h3 className="text-xl font-semibold text-darkBlue-900 dark:text-lightGray-800">
                   Border Countries:
                 </h3>
-                <div className="flex flex-wrap items-center gap-4">
+                <section className="flex flex-wrap items-center gap-4">
                   {borders
                     ? borders.map((border) => (
                         <button
                           key={border}
                           type="button"
-                          className="bg-slate-50 py-1 px-4 shadow-xl"
+                          className="bg-slate-200 py-1 px-4 shadow-xl"
                         >
                           {border}
                         </button>
                       ))
                     : 'N/A'}
-                </div>
+                </section>
               </div>
-            </article>
-          )
-        )}
-      </section>
+            </div>
+          </section>
+        )
+      )}
     </>
   );
 }
