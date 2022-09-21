dcf14348b044a6369c0b47ac69939448c4e59f1d
Victoria Cheng <victoria.cheng15@gmail.com>
2022-09-21 14:18:23 -0600

    refactor: adjust display page size for responsiveness

--- src/componments/Pagination/index.jsx
+++ src/componments/Pagination/index.jsx
@@ -1,29 +1,29 @@
 import React, { useContext } from 'react';
 import { RiArrowDropLeftLine, RiArrowDropRightLine } from 'react-icons/ri';
-import PaginationContext from '../../context/PaginationContext';
 import Button from '../Button';
+import PaginationContext from '../../context/PaginationContext';
 
 function Pagination() {
   const { currentPage, displayPages, handleNext, handlePrev } =
     useContext(PaginationContext);
 
   return (
-    <section className="flex flex-wrap items-center justify-center gap-4">
+    <section className="flex items-center justify-center gap-4">
       <Button onClick={() => handlePrev(currentPage)}>
         <RiArrowDropLeftLine />
       </Button>
-      <div className="flex w-[460px] items-center justify-center gap-8">
+      <div className="flex w-[460px] flex-wrap items-center justify-center gap-6 md:gap-8">
         {displayPages.map((num) =>
           currentPage === num ? (
             <span
-              className="grid aspect-square w-16 place-items-center border-4 border-lightGray-900 text-2xl font-bold dark:border-lightGray-700 dark:text-lightGray-800"
+              className="grid aspect-square w-10 place-items-center border-2 border-lightGray-900 text-2xl font-bold dark:border-lightGray-700 dark:text-lightGray-800 md:w-12"
               key={num}
             >
               {num}
             </span>
           ) : (
             <span
-              className="grid aspect-square w-14 place-items-center border-4 border-darkBlue-700 text-2xl font-bold dark:border-lightGray-900 dark:text-lightGray-700"
+              className="grid aspect-square w-8 place-items-center border-2 border-darkBlue-700 text-2xl font-bold dark:border-lightGray-900 dark:text-lightGray-700 md:w-10"
               key={num}
             >
               {num}
