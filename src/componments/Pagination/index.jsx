7ae3541b921ff623eb5a95320da4d3e23336edc2
Victoria Cheng <victoria.cheng15@gmail.com>
2022-09-22 16:15:11 -0600

    fix: use scale to show which page the user is on at this moment

--- src/componments/Pagination/index.jsx
+++ src/componments/Pagination/index.jsx
@@ -16,14 +16,14 @@ function Pagination() {
         {displayPages.map((num) =>
           currentPage === num ? (
             <span
-              className="grid aspect-square w-12 place-items-center border-4 border-lightGray-900 text-2xl font-bold dark:border-lightGray-700 dark:text-lightGray-800 md:w-14"
+              className=" grid aspect-square w-12 scale-125 place-items-center border-4 border-lightGray-900 text-2xl font-bold dark:border-lightGray-700 dark:text-lightGray-800 md:w-14"
               key={num}
             >
               {num}
             </span>
           ) : (
             <span
-              className="grid aspect-square w-10 place-items-center border-4 border-darkBlue-700 text-2xl font-bold dark:border-lightGray-900 dark:text-lightGray-700 md:w-12"
+              className="grid aspect-square w-12 place-items-center border-4 border-darkBlue-700 text-2xl font-bold dark:border-lightGray-900 dark:text-lightGray-700 md:w-14"
               key={num}
             >
               {num}
