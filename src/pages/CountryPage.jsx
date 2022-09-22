005e10a883d59913ebb268bb41af40d140345242
Victoria Cheng <victoria.cheng15@gmail.com>
2022-09-22 12:35:55 -0600

    fix: get name for border countries and style

--- src/pages/CountryPage.jsx
+++ src/pages/CountryPage.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { IoArrowBackOutline } from 'react-icons/io5';
 import useFetchCountry from '../hook/useFetchCountry';
+import CountriesContext from '../context/CountriesContext';
 import Spinner from '../componments/CountryCard/Spinner';
 import Container from '../componments/Container';
 import Paragraph from '../componments/Paragraph';
@@ -9,13 +10,20 @@ import Paragraph from '../componments/Paragraph';
 function CountryPage() {
   const { code } = useParams();
   const { country, loading } = useFetchCountry(code);
+  const { list } = useContext(CountriesContext);
   const getInput = (input) => Object.values(input);
+
+  const getBorderName = (border) => {
+    const getName = list.filter(({ cca3 }) => cca3.includes(border));
+
+    return getName.map(({ name }) => name.common)[0];
+  };
 
   if (loading) return <Spinner />;
 
   return (
     <>
-      <Link to="/" className="">
+      <Link to="/">
         <div className="inline-flex items-center gap-4 rounded-lg border-2 border-gray-600 p-2 font-semibold text-darkBlue-900 duration-300 ease-in-out hover:bg-darkBlue-900 hover:text-lightGray-700 dark:border-gray-300  dark:text-lightGray-800 dark:hover:bg-lightGray-700 dark:hover:text-darkBlue-700">
           <IoArrowBackOutline /> back
         </div>
@@ -35,7 +43,7 @@ function CountryPage() {
         }) => (
           <section
             key={name.common}
-            className="mt-20 flex w-full flex-col items-center justify-center gap-10 lg:flex-row"
+            className="mt-20 flex w-full flex-col justify-center gap-10 lg:flex-row"
           >
             <img
               className="w-full max-w-[550px] flex-1 border-2 border-lightGray-900"
@@ -79,18 +87,18 @@ function CountryPage() {
                 <h3 className="text-xl font-semibold text-darkBlue-900 dark:text-lightGray-800">
                   Border Countries:
                 </h3>
-                <section className="flex flex-wrap items-center gap-4">
+                <section className="flex flex-wrap items-center gap-4 text-darkBlue-900 dark:text-lightGray-800">
                   {borders
                     ? borders.map((border) => (
-                        <button
+                        <Link
+                          to={`/country/${border}`}
                           key={border}
-                          type="button"
-                          className="bg-slate-200 py-1 px-4 shadow-xl"
+                          className="bg-lightGray-800 py-1 px-3 text-lg shadow-md shadow-darkBlue-900 dark:bg-darkBlue-900 dark:text-lightGray-800 dark:shadow-lightGray-900"
                         >
-                          {border}
-                        </button>
+                          {getBorderName(border)}
+                        </Link>
                       ))
-                    : 'N/A'}
+                    : 'No Border Countries'}
                 </section>
               </div>
             </div>
