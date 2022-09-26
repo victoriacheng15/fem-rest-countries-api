92811a8e8155d14c02354fa691f4cf8ff5fc3023
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2022-09-26 14:30:19 -0600

    Fix/padding in header (#51)
    * chore: increase padding for mobile and reudce padding for desktop
    
    * chore: set card width and height for mobile and desktop versions and add margin for h2

--- src/componments/CountryCard/index.jsx
+++ src/componments/CountryCard/index.jsx
@@ -13,13 +13,13 @@ function CountryCard({
 }) {
   return (
     <Link
-      className="flex h-[400px] basis-72 flex-col gap-4 overflow-hidden rounded-lg bg-lightGray-800 shadow-lg shadow-gray-600 duration-300 ease-in-out hover:scale-110 dark:bg-darkBlue-700 dark:shadow-gray-600"
+      className="flex h-[420px] basis-80 flex-col gap-4 overflow-hidden rounded-lg bg-lightGray-800 shadow-lg shadow-gray-600 duration-300 ease-in-out hover:scale-110 dark:bg-darkBlue-700 dark:shadow-gray-600 lg:basis-72"
       to={`/country/${alphaCode}`}
     >
       <article>
         <img src={flags} alt={alt} className="object-fit h-[180px] w-full" />
         <div className="flex flex-col gap-4 p-3">
-          <h2 className="text-lg font-extrabold text-darkBlue-900 dark:text-lightGray-800">
+          <h2 className="my-2 text-lg font-extrabold text-darkBlue-900 dark:text-lightGray-800">
             {name}
           </h2>
           <div className="flex flex-col gap-2">
