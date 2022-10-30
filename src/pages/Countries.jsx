91ca7de0a49c9cb80dd33c578b6f51dfe34c22ac
Victoria Cheng <victoria.cheng15@gmail.com>
2022-10-30 13:10:10 -0600

    feat: import error and display ErrorPage if error happens

--- src/pages/Countries.jsx
+++ src/pages/Countries.jsx
@@ -2,17 +2,18 @@ import React, { useContext } from 'react';
 import CountriesContext from '../context/CountriesContext';
 import PaginationContext from '../context/PaginationContext';
 import { CountryCard, Form, Pagination, Spinner } from '../components';
+import { ErrorPage } from '.';
 
 function Countries() {
-  const { loading } = useContext(CountriesContext);
+  const { loading, error } = useContext(CountriesContext);
   const { currentCountries } = useContext(PaginationContext);
-
-  if (loading) return <Spinner />;
 
   return (
     <>
       <Form />
       <section className="my-12 flex flex-wrap items-center justify-around gap-8">
+        {error && <ErrorPage />}
+        {loading && <Spinner />}
         {currentCountries.map(
           ({ name, capital, region, flags, population, cca2 }) => (
             <CountryCard
