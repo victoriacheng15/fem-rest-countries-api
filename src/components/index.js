6262bbc4f22e96b9e6317d1d13efd1e02009bcb7
Victoria Cheng <victoria.cheng15@gmail.com>
2022-11-18 21:22:18 -0700

    chore: reorganize the list of exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,16 @@
 /* eslint-disable import/no-cycle */
-export { default as Button } from './Button';
-export { default as BackHome } from './BackHome';
-export { default as CountryCard } from './CountryCard';
-export { default as Footer } from './Footer';
-export { default as Form } from './Form';
 export { default as Header } from './Header';
-export { default as Main } from './Main';
+export { default as Footer } from './Footer';
+
+export { default as Form } from './Form';
+export { default as CountryCard } from './CountryCard';
 export { default as Pagination } from './Pagination';
-export { default as Paragraph } from './Paragraph';
 export { default as Spinner } from './Spinner';
+export { default as Paragraph } from './Paragraph';
+export { default as BackHome } from './BackHome';
+export { default as Button } from './Button';
+
 export { default as FlagImage } from './PageComp/FlagImage';
 export { default as Heading2 } from './PageComp/Heading2';
 export { default as Heading3 } from './PageComp/Heading3';
+export { default as BorderButton } from './PageComp/BorderButton';
